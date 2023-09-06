441ca1a4ef9a3ad1a35e90ff4fb46261db744b4c
Sefa <sefa.kanbur@diva-e.com>
2023-09-06 15:06:44 +0200

    setup input modes

--- src/code-challange.ts
+++ src/code-challange.ts
@@ -1,7 +1,16 @@
 const errorMessage = 'I have no idea what you are talking about'
+
+function checkIfQuestion(input: string): boolean {
+    return (input.startsWith('how much') || input.startsWith('how many')) && input.endsWith('?')
+}
+
 export function handleInput(input: string): string | unknown {
     try {
+        if(!checkIfQuestion(input)) {
+        } else if(input.startsWith('how much')) {
 
+        } else if (input.startsWith('how many')) {
+        }
     } catch (e) {
         return errorMessage
     }
