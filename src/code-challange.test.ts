1760fad9242ee41b3c56846ce26cfaa067eb95f3
Sefa <sefa.kanbur@diva-e.com>
2023-09-08 13:43:36 +0200

    refactor unit tests

--- src/code-challange.test.ts
+++ src/code-challange.test.ts
@@ -1,7 +1,7 @@
 import { handleInput} from "./code-challange";
 
-describe("Math functions", () => {
-    it("should multiply 5 by 3", () => {
+describe("Code Challange", () => {
+    beforeEach(() => {
         handleInput('glob is I');
         handleInput('prok is V');
         handleInput('pish is X');
@@ -9,11 +9,20 @@ describe("Math functions", () => {
         handleInput('glob glob Silver is 34 Credits');
         handleInput('glob prok Gold is 57800 Credits');
         handleInput('pish pish Iron is 3910 Credits');
-
+    })
+    it("should return arabic number", () => {
         expect(handleInput('how much is pish tegj glob glob ?')).toEqual('pish tegj glob glob is 42');
+    });
+    it("should return price of Silver", () => {
         expect(handleInput('how many Credits is glob prok Silver ?')).toEqual('glob prok Silver is 68 Credits');
+    });
+    it("should return price of Gold", () => {
         expect(handleInput('how many Credits is glob prok Gold ?')).toEqual('glob prok Gold is 57800 Credits');
+    });
+    it("should return price of Iron", () => {
         expect(handleInput('how many Credits is glob prok Iron ?')).toEqual('glob prok Iron is 782 Credits');
+    });
+    it("should handle wrong inputs", () => {
         expect(handleInput('how much wood could a woodchuck chuck if a woodchuck could chuck wood ?')).toEqual('I have no idea what you are talking about');
     });
 });
